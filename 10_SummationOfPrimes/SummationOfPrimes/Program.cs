d9ecd5a81e785f2f512df7bdcb7f51eccd3ef13a
xszaboj <xszaboj@centrum.cz>
2015-07-24 09:11:17 +0100

    number 10 solved

---
+++ 10_SummationOfPrimes/SummationOfPrimes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummationOfPrimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 17;
+            for (int i = 10; i < 2000000; i++)
+            {
+                if (IsPrimeNumber(i))
+                {
+                    sum = sum + i;
+                }
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+
+        private static bool IsPrimeNumber(int number)
+        {
+            var sqrt = Math.Sqrt(number);
+            for (int i = 2; i <= sqrt; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
