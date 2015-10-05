84a67ceab85393a388b0893aacbc0876fcfed889
Jack <xszaboj@gmail.com>
2015-10-05 14:14:09 +0100

    multiply refactored

--- PowerOfTwo/PowerOfTwo/Program.cs
+++ PowerOfTwo/PowerOfTwo/Program.cs
@@ -22,46 +22,95 @@ namespace PowerOfTwo
 
             for (int i = numberTwoSize -1; i >= 0; i--)
             {
-                var addNulls = numberTwoSize - (i + 1);
                 var result = "";
-                if (addNulls > 0)
-                {
-                    for (int k = 0; k < addNulls; k++)
-                    {
-                        result = "0" + result;
-                    }
-                }
+                result = AddNulls(result, HowManyNullsToAdd(numberTwoSize, i));
                 var remainder = 0;
                 for (int j = numberOneSize-1; j >= 0; j--)
                 {
-                    int multiply = two.SequenceOfNumbers[i] * one.SequenceOfNumbers[j];
-                    multiply += remainder;
-                    remainder = 0;
-                    int numberToWrite = 0;
-                    if (j == 0)
-                    {
-                        numberToWrite = multiply;
-                    }
-                    else
-                    {
-                        if (multiply >= 10)
-                        {
-                            numberToWrite = multiply % 10;
-                            remainder = multiply / 10;
-                        }
-                        else
-                        {
-                            numberToWrite = multiply;
-                        }
-                    }
-                    result = numberToWrite + result;
+                    int multiply = Multiply(two.SequenceOfNumbers[i], one.SequenceOfNumbers[j]);
+                    multiply = AddRemainder(multiply, remainder);
+                    var numberToWrite = GetNumberToWrite(j, multiply);
+                    remainder = GetRemainder(j, multiply);
+                    result = PrependToResult(result, numberToWrite.ToString());
                 }
-                results.Add(result);
-                result = "";
+                AddCurrentResultToResults(result, results);
+                ClearResult(result);
             }
-
-
             return null;
+        }
+
+        private static string AddNulls(string result, int howManyNullsToAdd)
+        {
+            if (howManyNullsToAdd > 0)
+            {
+                for (int i = 0; i < howManyNullsToAdd; i++)
+                {
+                    result = PrependToResult(result, "0");
+                }
+            }
+            return result;
+        }
+        private static int HowManyNullsToAdd(int numberTwoSize, int loopNumber)
+        {
+            return numberTwoSize - (loopNumber + 1);
+        }
+        private static int Multiply(int numberOne, int numberTwo)
+        {
+            return numberOne * numberTwo;
+        }
+        private static int AddRemainder(int number, int remainder)
+        {
+            return number += remainder;
+        }
+        private static int GetNumberToWrite(int loopNumber, int number)
+        {
+            int numberToWrite = 0;
+            if (loopNumber == 0)
+            {
+                numberToWrite = number;
+            }
+            else
+            {
+                if (number >= 10)
+                {
+                    numberToWrite = number % 10;
+                }
+                else
+                {
+                    numberToWrite = number;
+                }
+            }
+            return numberToWrite;
+        }
+        private static int GetRemainder(int loopNumber, int number)
+        {
+            if (loopNumber == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                if (number >= 10)
+                {
+                    return  number / 10;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+        private static string PrependToResult(string result, string prepend)
+        {
+            return prepend + result;
+        }
+        private static void AddCurrentResultToResults(string currentResult, List<string> results)
+        {
+            results.Add(currentResult);
+        }
+        private static void ClearResult(string result)
+        {
+            result = "";
         }
     }
 }
