d3eef53232ab6c1c568695e0dda4abe0e8720f5b
Jack <xszaboj@gmail.com>
2015-12-29 14:15:40 +0000

    20 solved

---
+++ 20_Factorial/Factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate c = new Calculate();
+            BigInteger result = c.CalculateFactorial(100);
+            double sum = 0;
+            char[] array = result.ToString().ToArray<char>();
+            foreach (char number in array)
+            {
+                sum += Char.GetNumericValue(number);
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 20_Factorial/Test/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Factorial;
+using System.Numerics;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Calculate c = new Calculate();
+            var result = c.CalculateFactorial(3);
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Calculate c = new Calculate();
+            var result = c.CalculateFactorial(10);
+            Assert.AreEqual(3628800, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Calculate c = new Calculate();
+            var result = c.CalculateFactorial(20);
+            Assert.AreEqual(new BigInteger((ulong)2432902008176640000), result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Calculate c = new Calculate();
+            BigInteger result = c.CalculateFactorial(100);
+            result.ToString();
+
+        }
+    }
+}
