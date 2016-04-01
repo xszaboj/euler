f9d91b14e78dac6caa25bc84ae4486655f3bd413
Jack <xszaboj@gmail.com>
2016-04-01 14:29:04 +0100

    29 solved

---
+++ 29_DistinctPowers/DistinctPowers/CompositeNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistinctPowers
+{
+    public class CompositeNumber
+    {
+        private int a;
+        private int minb;
+        private int maxb;
+
+
+        public CompositeNumber(int a, int minb, int maxb)
+        {
+            this.a = a;
+            this.minb = minb;
+            this.maxb = maxb;
+        }
+
+        public List<double> CalculatePowers()
+        {
+            var list = new List<double>();
+            for (int b = minb; b <= maxb; b++)
+            {
+                var result = Math.Pow(a, b);
+                list.Add(result);
+            }
+            return list;
+        }
+    }
+}
---
+++ 29_DistinctPowers/Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DistinctPowers;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var composite = new CompositeNumber(2, 2, 5);
+            var result = composite.CalculatePowers();
+            Assert.AreEqual(4, result[0]);
+            Assert.AreEqual(8, result[1]);
+            Assert.AreEqual(16, result[2]);
+            Assert.AreEqual(32, result[3]);
+        }
+    }
+}
